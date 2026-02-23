5fde8cde0a421b0431bcf9924b87cf3b5541521d
Vinicius Leonardo Silveira <vinicius.silveira1503@gmail.com>
2026-02-23 11:07:27 -0300

    aula2

---
+++ aula2-OOP+Heranca/OOPaula1/OOPaula1/Produto2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPaula1
+{
+    class Produto2
+    {
+        public Produto2(string nome, double preco, int estoqueminimo)
+        {
+            Nome = nome;
+            Preco = preco;
+            EstoqueMinimo = estoqueminimo;
+        }
+
+        // se nao retornar nada usa void
+        public void Exibir()
+        {
+            Console.WriteLine($"{Nome}\n{Preco.ToString("c")}\n{PrecoFinal.ToString("c")}");
+        }
+
+        private string? nome;
+        // para fazer alteracoes no get/set tem que definir como private
+
+        public string? Nome
+        {
+            get { return nome?.ToUpper(); }
+            set { nome = value; }
+        }
+
+        private double preco;
+        public double Preco
+        { 
+            get { return preco; }
+            set { 
+                if (preco > 5.00) {
+                    preco = 5.00;
+                } else
+                {
+                    preco = value;
+                }
+            } 
+        }
+
+        private double desconto = 0.05;
+        public double Desconto 
+        { 
+            get { return desconto; } 
+        }
+
+        public double PrecoFinal 
+        { 
+            get { return Preco - (Preco * Desconto); }
+        }
+
+        private int minimo;
+        public int EstoqueMinimo
+        {
+            set { minimo = value; }
+        }
+
+    }
+}
---
+++ aula2-OOP+Heranca/OOPaula1/OOPaula1/Program.cs
@@ -0,0 +1,52 @@
+﻿
+// ==============
+//     usando
+// ==============
+
+
+using OOPaula1;
+
+Produto prod1 = new Produto();
+prod1.Nome = "Caderno";
+prod1.Preco = 50.90;
+
+Produto prod2 = new Produto("Lápis", 1.50);
+
+Console.WriteLine("Nome: " + prod1.Nome); // concatenacao
+Console.WriteLine($"Preco: {prod1.Preco}"); // interpolacao
+
+Console.WriteLine();
+
+Console.WriteLine($"Nome: {prod2.Nome}");
+Console.WriteLine($"Preco: {prod2.Preco}");
+
+Console.WriteLine();
+Console.WriteLine("==========================================");
+Console.WriteLine();
+
+// ===================
+//   Produto2 Objeto
+// ===================
+
+Produto2 prod3 = new("Borracha", 4.60, 100);
+prod3.Exibir();
+
+Console.ReadKey();
+// ==============
+//   declarando
+// ==============
+
+
+public class Produto
+{
+    public Produto() { }// definir um construtor padrao (sem parametros) para nao ter erro no prod1, que tem seus atributos definidos depois
+
+    public Produto(string nome, double Preco)
+    {
+        Nome = nome; // definindo nome como nome
+        this.Preco = Preco; // em caixa alta precisa de atributo
+    }
+
+    public string? Nome { get; set; }
+    public double Preco { get; set; }
+}
