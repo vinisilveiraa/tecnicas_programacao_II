13b88679738710254cb966b49faca76ae0f7743a
Vinicius Leonardo Silveira <vinicius.silveira1503@gmail.com>
2026-05-18 11:09:51 -0300

    todo aula1

---
+++ TodoApi/TodoApi/Controllers/TarefasController.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.InteropServices;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+using TodoApi.Repositories;
+
+namespace TodoApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TarefasController : Controller
+    {
+        private readonly TarefaRepository _repository;
+        public TarefasController(TarefaRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Tarefa>>> Get()
+        {
+            var tarefas = await _repository.ObterTodasTarefasAsync();
+            return Ok(tarefas);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Tarefa novaTarefa)
+        {
+            await _repository.CriarTarefaAsync(novaTarefa);
+            return CreatedAtAction(nameof(Get), new { id = novaTarefa.Id }, novaTarefa);
+        }
+    }
+}
---
+++ TodoApi/TodoApi/Repositories/TarefaRepository.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+using TodoApi.Models;
+
+namespace TodoApi.Repositories
+{
+    public class TarefaRepository
+    {
+        private readonly IMongoCollection<Tarefa> _tarefasCollection;
+
+        public TarefaRepository(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("MongoConnection");
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("TodoDatabase");
+            _tarefasCollection = database.GetCollection<Tarefa>("Tarefas");
+        }
+
+        public async Task<List<Tarefa>> ObterTodasTarefasAsync() =>
+            await _tarefasCollection.Find(_ => true).ToListAsync();
+
+        public async Task CriarTarefaAsync(Tarefa novaTarefa) =>
+            await _tarefasCollection.InsertOneAsync(novaTarefa);
+
+
+    }
+}
